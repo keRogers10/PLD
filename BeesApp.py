68d5b208027d1f226e54e9485258f3e452ddd96f
YOUR <r.murara@alustudent.com>
2023-08-17 16:47:56 -0700

    run

---
+++ BeesApp.py
@@ -0,0 +1,62 @@
+import json
+
+# Load businesses from json file
+with open('businesses.json', 'r') as f:
+    businesses = json.load(f)
+
+def add_business():
+    new_business = {}
+    new_business['name'] = input("Enter the name of the business: ")
+    new_business['description'] = input("Enter a description of the business: ")
+    new_business['address'] = input("Enter the address of the business: ")
+    new_business['phone'] = input("Enter the phone number of the business: ")
+    new_business['website'] = input("Enter the website of the business: ")
+    print('Business added')
+    
+    # Generate a new unique ID for the business
+    new_id = str(max([int(key) for key in businesses.keys()]) + 1)
+    
+    # Add the new business to the dictionary
+    businesses[new_id] = new_business
+    
+    # Save businesses to json file
+    with open('businesses.json', 'w') as f:
+        json.dump(businesses, f)
+        
+def delete_business():
+    name = input("Enter the name of the business you want to delete: ")
+    for key, business in businesses.items():
+        if business['name'] == name:
+            del businesses[key]
+            print(f"{name} has been deleted.")
+            break
+    else:
+        print(f"{name} not found.")
+    
+def display_businesses():
+    for key, business in businesses.items():
+        print(f"ID: {key}")
+        print(f"Name: {business['name']}")
+        print(f"Description: {business['description']}")
+        print(f"Address: {business['address']}")
+        print(f"Phone: {business['phone']}")
+        print(f"Website: {business['website']}")
+        print()
+        
+while True:
+    print("1. Add a business")
+    print("2. Delete a business")
+    print("3. Display all businesses")
+    print("4. Quit")
+    choice = input("Enter your choice: ")
+    
+    if choice == '1':
+        add_business()
+    elif choice == '2':
+        delete_business()
+    elif choice == '3':
+        display_businesses()
+    elif choice == '4':
+        break
+    else:
+        print("Invalid choice. Please try again.")
---
+++ BeesApp1.py
@@ -0,0 +1,54 @@
+import json
+import mysql.connector
+
+# Load businesses from JSON file
+with open('businesses.json', 'r') as f:
+    businesses = json.load(f)
+
+# ... Other functions ...
+
+def add_business():
+    # Collect business details from user
+    new_business = {}
+    new_business['name'] = input("Enter the name of the business: ")
+    new_business['description'] = input("Enter a description of the business: ")
+    new_business['address'] = input("Enter the address of the business: ")
+    new_business['phone'] = input("Enter the phone number of the business: ")
+    new_business['website'] = input("Enter the website of the business: ")
+    
+    # ... Add other details ...
+    
+    # Add the new business to the dictionary
+    new_id = str(max([int(key) for key in businesses.keys()]) + 1)
+    businesses[new_id] = new_business
+    
+    # Save businesses to JSON file
+    with open('businesses.json', 'w') as f:
+        json.dump(businesses, f)
+    
+    # Insert data into MySQL database
+    try:
+        connection = mysql.connector.connect(
+            host="remote_host",
+            user="root@localhost",
+            password=" ",
+            database="Business_information_hub"
+        )
+        cursor = connection.cursor()
+
+        sql_insert_query = "INSERT INTO businesses (name, description, address, phone, website) VALUES (%s, %s, %s, %s, %s)"
+        insert_data = (new_business['name'], new_business['description'], new_business['address'], new_business['phone'], new_business['website'])
+
+        cursor.execute(sql_insert_query, insert_data)
+        connection.commit()
+        print("Business added and data inserted into MySQL database successfully!")
+
+    except mysql.connector.Error as error:
+        print("Error: {}".format(error))
+    
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+while True:
